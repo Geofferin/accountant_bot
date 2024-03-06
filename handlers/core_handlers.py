2b4a2fae09967bb62bc9a1a922c61c10a82589df
Geofferin <syanczov@bk.ru>
2024-03-07 00:01:07 +0300

    теперь отслеживается длинна только одной категории. Если ты введешь две нормальные категории и одну слишком длинную, то нормальные запишутся, а про длинную бот выдаст ошибку. При этом так же учитывается разница в длинне между обычными символами и эмоджи
    Добавил проверку на дублирующиеся категории
    
    Добавил обработку при вводе слишком большого кол-ва дней для истории. Максимум 36500 дней.
    
    Сделал адекватное отображение инлайн клавиатуры. Теперь категории будут отображаться всегда по 3. Кнопки управления всегда идут отдельно ниже.
    
    Заменил английский текст сообщений на русский.
    Сделал удаление лишних сообщений.
    Хотел добавить описание, но мне лень расписывать)

--- handlers/core_handlers.py
+++ handlers/core_handlers.py
@@ -2,6 +2,8 @@ from aiogram import F, types
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
+import emoji
+import asyncio
 
 from database.database import Database
 from keyboards import keyboards
@@ -16,30 +18,21 @@ TO DO:
     вполне себе можно так сделать, через переменные среды - туда на обоих компьютерах сохраняем инфу, которая бы определяла наши компы
     а бот при запуске эту инфу чекает и подгружает нужный вариант токена
     
-    СДЕЛАТЬ ВСЕ БАЗОВЫЕ ФУНКЦИИ, ЗАБИНДИТЬ ВСЕ КНОПКИ - DONE
-    ДОБАВИТЬ ИСТОЧНИКИ ДОХОДА И СТАТЬИ РАСХОДА:
-        список должен открываться после кнопки earned или spent, последний пункт в списке должен позволять добавление нового источника или статьи
-        1. добавить столбцы для категорий в обе таблицы - DONE
-        2. ВРЕМЕННО. сделать запись тестовой категории, иначе бот не будет работать. - DONE
-        3. добавить клавиатуру со стандартными категориями, переписать логику так, чтобы сначала открывалась она и после выбора категории можно было ввести сумму - DONE
-        4. прописать возможность добавлять свои категории - DONE
-        5. при выводе истории должны запрашиваться конкретные категории, записи по которым и будут выводиться. Но также оставить возможность выводить всю историю.
+    При выводе истории должны запрашиваться конкретные категории, записи по которым и будут выводиться. Но также оставить возможность выводить всю историю.
     ДОБАВИТЬ НЕОБЯЗАТЕЛЬНОЕ ОПИСАНИЕ ЗАПИСИ. П-ЛЬ МОЖЕТ ДАТЬ ОПИСАНИЕ, А МОЖЕТ НЕ ДАВАТЬ.
     
 Баги:
     Критические:
-        бот сыпется, если слишком большая длина одной категории. сделать обработку
+        бот сыпется, если слишком большая длина одной категории. сделать обработку - DONE
         убрать глобальные переменные - DONE
-        изначально должны вводиться базовые категории, иначе будет None и его нельзя будет убрать
     Остальные:
-        запретить добавление дублирующей категории
+        запретить добавление дублирующей категории - DONE
+        Проверять длинну категорий по отдельности, а не длинну всего сообщения - DONE
+        обработать исключение для ввода слишком большого кол-ва дней, за которые должна выводиться история - DONE
         Если кто то добавит категорию с названием начинающимся с del_, то вместо записи, категория удалиться
-        Проверяет длину категорий целиком, будет ошибка если ввести несколько категорий на общую длину 44 символа, а запрет должен касаться только длинны одной категории.
-        не дать ввести команду в качестве названия категории
-        обработать исключение для ввода слишком большого кол-ва дней, за которые должна выводиться история
     Полировка:
-        заменить технические названия и сделать вывод описания в начале, сделать удаление сообщений с командами
-        сделать код более читаемым. Разгрузить колбек хендлер и основной файл. Состояния в отдельный файл
+        заменить технические названия и сделать вывод описания в начале, сделать удаление сообщений с командами - DONE
+        красиво расположить кнопки на инлайн клавиатуре - DONE
 """
 
 router = Router()
@@ -50,8 +43,14 @@ BotDB = Database()
 async def start(message: types.Message, state: FSMContext):
     if not BotDB.user_exists(message.from_user.id):
         BotDB.add_user(message.from_user.id)
+    await message.delete()
     await state.set_state(States.default)
-    await message.bot.send_message(message.from_user.id, "Добро пожаловать!", reply_markup = main_menu_kb())
+    await message.bot.send_message(message.from_user.id, "Добро пожаловать!", reply_markup=main_menu_kb())
+
+
+@router.message(F.text.in_("Помощь"))
+async def help(message: types.Message, state: FSMContext) -> None:
+    await message.answer('Что то тут наверное стоило написать...')
 
 
 @router.message(~StateFilter(States.default), Command('cancel'))
@@ -60,77 +59,93 @@ async def cancel(message: types.Message, state: FSMContext) -> None:
     await start(message, state)
 
 
-@router.message(Command("Баланс"))
+@router.message(F.text.in_("Баланс"))
 async def balance(message: types.Message, state: FSMContext) -> None:
-    balance = BotDB.get_balance(user_id = message.from_user.id)
+    balance = BotDB.get_balance(user_id=message.from_user.id)
     await message.answer(f'Ваш текущий баланс: {round(balance, 2)} ₽')
+    await message.delete()
 
 
-@router.message(Command("История"))
+@router.message(F.text.in_("История"))
 async def history(message: types.Message, state: FSMContext) -> None:
-    await message.bot.send_message(message.from_user.id, "Enter the number of days for which the transaction history is needed")
+    msg = await message.bot.send_message(message.from_user.id, 'Введите количество дней, за которые необходимо получить историю записей')
+    await message.delete()
     await state.set_state(States.input_date)
+    asyncio.create_task(delete_message(msg, 10))
 
 
 @router.message(F.text.in_(["Записать доход", "Записать расход"]))
 async def note(message: types.Message, state: FSMContext) -> None:
     if message.text == "Записать доход":
-        await state.update_data(operation = 'income')
-        operation = True
-        categories = BotDB.get_categories(user_id = message.from_user.id, operation = 'income')
+        await state.update_data(operation='income')
+        categories = BotDB.get_categories(user_id=message.from_user.id, operation='income')
     elif message.text == "Записать расход":
-        await state.update_data(operation = 'spend')
-        operation = False
-        categories = BotDB.get_categories(user_id = message.from_user.id, operation = 'spend')
-    await message.bot.send_message(message.from_user.id, "Выберите категорию", reply_markup = keyboards.get_inline_keyboard(categories))
+        await state.update_data(operation='spend')
+        categories = BotDB.get_categories(user_id=message.from_user.id, operation='spend')
+    await message.delete()
+    await message.bot.send_message(message.from_user.id, "Выберите категорию", reply_markup=keyboards.get_inline_keyboard(categories))
 
 
 @router.callback_query()
 async def vote_callback(callback: types.CallbackQuery, state: FSMContext):
     action = await state.get_data()
     if callback.data == 'add':
-        await callback.bot.send_message(chat_id = callback.message.chat.id,
-                                        text = 'Введите название категории\nЕсли их несколько, вводите через пробел')
+        msg = await callback.bot.send_message(chat_id=callback.message.chat.id, text='Введите название категории\nЕсли их несколько, вводите через пробел')
         await state.set_state(States.input_new_category)
+        asyncio.create_task(delete_message(msg, 10))
 
     elif callback.data == 'remove':
         if action['operation'] == 'income':
-            categories = BotDB.get_categories(user_id = callback.from_user.id, operation = 'income')
+            categories = BotDB.get_categories(user_id=callback.from_user.id, operation='income')
         elif action['operation'] == 'spend':
-            categories = BotDB.get_categories(user_id = callback.from_user.id, operation = 'spend')
-        await callback.bot.send_message(chat_id = callback.message.chat.id, text = 'Какую категорию удалять?',
-                                        reply_markup = keyboards.del_category(categories))
+            categories = BotDB.get_categories(user_id=callback.from_user.id, operation='spend')
+        await callback.bot.send_message(chat_id=callback.message.chat.id, text='Какую категорию удалять?', reply_markup=keyboards.del_category(categories))
 
-    elif callback.data[
-         :4] == 'del_':  # Если кто то добавит категорию с названием начинающимся с del_, то вместо записи, категория удалиться
-        BotDB.del_category(user_id = callback.from_user.id, category_del = callback.data.replace('del_', ''),
-                           operation = action['operation'])
-        await callback.bot.send_message(chat_id = callback.message.chat.id, text = 'Категория удалена')
+    elif callback.data[:4] == 'del_':  # Если кто то добавит категорию с названием начинающимся с del_, то вместо записи, категория удалиться
+        BotDB.del_category(user_id=callback.from_user.id, category_del=callback.data.replace('del_', ''), operation=action['operation'])
+        await callback.bot.send_message(chat_id=callback.message.chat.id, text='Категория удалена')
 
     elif callback.data == 'exit':
         pass
 
     else:
-        await state.update_data(category = callback.data)
-        await callback.bot.send_message(chat_id = callback.message.chat.id, text = 'Введите сумму')
+        await state.update_data(category=callback.data)
+        msg = await callback.bot.send_message(chat_id=callback.message.chat.id, text='Введите сумму')
         await state.set_state(States.input_money)
+        asyncio.create_task(delete_message(msg, 10))
     await callback.message.delete()
 
 
 @router.message(StateFilter(States.input_new_category))
 async def add_category(message: types.Message, state: FSMContext) -> None:
     action = await state.get_data()
-    if len(message.text) < 44:  # Проверяет длинну категории целиком, будет ошибка если ввести несколько категорий на общую длинну 44 символа
-        for i in message.text.split():  # Специально написал i, потому что в прошлый раз запутался из-за названия переменных
-            BotDB.add_category(message.from_user.id, i, operation = action['operation'])
-        await message.answer(f'Категории добавлены')
-    else:
-        await message.reply('Максимальная длинна названия - 43 символа')
+    for x in message.text.split():
+        x = x.replace("'", '').replace('\\', '')
+        if action['operation'] == 'income':  # запрашиваем список уже добавленных категорий
+            categories = BotDB.get_categories(user_id=message.from_user.id, operation='income')
+        elif action['operation'] == 'spend':
+            categories = BotDB.get_categories(user_id=message.from_user.id, operation='spend')
+        if x not in categories:
+            count = 0
+            for letter in x:  # каждое отдельное название категории разбиваем на символы, чтобы проверить, не являются ли они эмоджи
+                if emoji.is_emoji(letter):
+                    count += 3
+                else:
+                    count += 1
+            if count < 44:  # методом тыка, определил, что это максимальная длинна названия, которая не будет ломать бота. Один эмоджи примерно равен 3 обычным символам
+                BotDB.add_category(message.from_user.id, x, operation=action['operation'])
+                await message.answer(f'Категория: {x} добавлена')
+            else:
+                await message.reply('Слишком большая длинна категории.\nМаксимальная длинна - 43 символа или 14 эмоджи')
+        else:
+            await message.answer(f'Категория {x} уже есть в списке')
+    await message.delete()
 
 
 @router.message(StateFilter(States.input_money))
 async def load_note(message: types.Message, state: FSMContext) -> None:
     value = message.text.replace(',', '.')
+    await message.delete()
     try:
         float(value)
     except ValueError:
@@ -155,13 +170,19 @@ async def load_note(message: types.Message, state: FSMContext) -> None:
 @router.message(StateFilter(States.input_date))
 async def load_history(message: types.Message, state: FSMContext) -> None:
     period = message.text
+    await message.delete()
     try:
         int(period)
     except ValueError:
-        await message.reply('You must enter a number')
+        await message.reply('Нужно ввести число')
     else:
-        if int(period) <= 0:
-            await message.reply('The number of days must be greater than zero')
+        if int(period) <= 0 or int(period) > 36500:
+            await message.reply('Количество дней должно быть больше нуля, но меньше 100 лет')
         else:
-            history = BotDB.get_history(period, user_id = message.from_user.id)
+            history = BotDB.get_history(period, user_id=message.from_user.id)
             await message.answer(history)
+
+
+async def delete_message(message: types.Message, sleep_time):
+    await asyncio.sleep(sleep_time)
+    await message.delete()
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,35 +1,38 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 
+
 def main_menu_kb() -> InlineKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    button1 = KeyboardButton(text = "Записать доход")
-    button2 = KeyboardButton(text = "Записать расход")
-    button3 = KeyboardButton(text = "/help")
-    button4 = KeyboardButton(text = "/Баланс")
-    button5 = KeyboardButton(text = "/История")
+    button1 = KeyboardButton(text="Записать доход")
+    button2 = KeyboardButton(text="Записать расход")
+    button3 = KeyboardButton(text="Баланс")
+    button4 = KeyboardButton(text="История")
+    button5 = KeyboardButton(text="Помощь")
 
-    kb.row(button1, button2, width = 2)
-    kb.row(button4, width = 1)
-    kb.row(button3, button5, width = 2)
+    kb.row(button1, button2, width=2)
+    kb.row(button3, button4, width=2)
+    kb.row(button5, width=1)
 
     return kb.as_markup()
+
 
 def get_inline_keyboard(categories):
     inline_keyboard = InlineKeyboardBuilder()
     for i in categories:
-        inline_keyboard.add(InlineKeyboardButton(text = i, callback_data = i))
-    inline_keyboard.add(InlineKeyboardButton(text = 'Добавить категории', callback_data = 'add'))
-    if len(categories) > 0:
-        inline_keyboard.add(InlineKeyboardButton(text = 'Удалить категории', callback_data = 'remove'))
-    inline_keyboard.add(InlineKeyboardButton(text = 'Выход', callback_data = 'exit'))
+        inline_keyboard.add(InlineKeyboardButton(text=i, callback_data=i))
     inline_keyboard.adjust(3)
+    inline_keyboard.row((InlineKeyboardButton(text='Добавить категории', callback_data='add')), width=1)
+    if len(categories) > 0:
+        inline_keyboard.add(InlineKeyboardButton(text='Удалить категории', callback_data='remove'))
+    inline_keyboard.row((InlineKeyboardButton(text='Выход', callback_data='exit')), width=1)
     return inline_keyboard.as_markup()
 
 
 def del_category(categories):
     ikb_del = InlineKeyboardBuilder()
     for i in categories:
-        ikb_del.add(InlineKeyboardButton(text = i, callback_data = f'del_{i}'))
-    ikb_del.add(InlineKeyboardButton(text = 'Выход', callback_data = 'exit'))
+        ikb_del.add(InlineKeyboardButton(text=i, callback_data=f'del_{i}'))
+    ikb_del.adjust(3)
+    ikb_del.row((InlineKeyboardButton(text='Выход', callback_data='exit')), width=1)
     return ikb_del.as_markup()
